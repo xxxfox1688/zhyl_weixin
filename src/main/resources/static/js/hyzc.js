2f0eecf2d29a49472c9004af01cfe2f159a8050b
bwjf <544722253@qq.com>
2018-09-18 16:16:42 +0800

    1
    1

--- src/main/resources/static/js/hyzc.js
+++ src/main/resources/static/js/hyzc.js
@@ -2,19 +2,44 @@
  * 
  */
 
-
-function tj(){
-	var options=$("#sex_sel option:selected");
+function tj() {
+	var options = $("#sex_sel option:selected");
 	$("#sex").val(options.val());
 
-	if(trim($("#user_name").val()) == "") {$$.alert("请填写姓名"); return};
-	if(trim($("#phone").val()) == "") {$$.alert("请填写电话号码"); return};
-	if(trim($("#zjhm").val()) == "") {$$.alert("请填写证件号码"); return};
-	if(trim($("#dz").val()) == "") {$$.alert("请填写住址"); return};
-	if(trim($("#sex").val()) == "") {$$.alert("请选择性别"); return};
+	if (trim($("#user_name").val()) == "") {
+		$$.alert("请填写姓名");
+		$('#user_name').focus();
+		return
+	}
+	;
+	// if(trim($("#phone").val()) == "") {$$.alert("请填写电话号码"); return};
+	// 判断手机号码
+	if ($.trim($('#phone').val()).length == 0) {
+		$$.alert("请填写手机号码");
+		$('#phone').focus();
+		return;
+	} else {
+		if (isPhoneNo($.trim($('#phone').val())) == false) {
+			$$.alert("请填写有效的手机号码");
+			$('#phone').focus();
+			return;
+		}
+	}
+	// if(trim($("#zjhm").val()) == "") {$$.alert("请填写证件号码"); return};
+	if (trim($("#dz").val()) == "") {
+		$$.alert("请填写住址");
+		$('#dz').focus();
+		return
+	}
+	;
+	if (trim($("#sex").val()) == "") {
+		$$.alert("请选择性别");
+		return
+	}
+	;
 
 	$.ajax({
-		url : 'hyzc/addUser',
+		url : '/hyzc/addUser',
 		type : 'post',
 		data : $('#addUserForm').serialize(),
 		dataType : "json",
@@ -28,10 +53,16 @@ function tj(){
 	});
 }
 
-function go_zc(){
+function go_zc() {
 	document.forms[0].submit();
 }
 
 function trim(x) {
-    return x.replace(/^\s+|\s+$/gm,'');
+	return $.trim(x)
+}
+
+// 验证手机号
+function isPhoneNo(phone) {
+	var pattern = /^1[34578]\d{9}$/;
+	return pattern.test(phone);
 }
